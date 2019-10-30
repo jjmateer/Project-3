1572d181f274248eb2eb0e89a0d5da3886a829a5
John Mateer <jj.mateer@live.com>
2019-10-29 21:33:23 -0700

    New lint script. Hoping to resolve new error thrown in travis.ci.

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var path = require('path');
-var PORT = process.env.PORT || 3001;
-require('dotenv').config();
-var app = express();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
-app.listen(PORT, function () {
-  console.log('🌎 ==> API server now on port' + PORT);
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
