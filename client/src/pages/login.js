c0c752d1b22862e83809d3e4dd3d00ecb769d9e7
John Mateer <jj.mateer@live.com>
2019-10-31 01:10:28 -0700

    Added babel-eslint to dev dependencies.

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,35 +1,34 @@
-// import React, { Component } from "react";
-// import LoginForm from "../components/loginform";
+import React, { Component } from "react";
+import LoginForm from "../components/loginform";
 
-// class Login extends Component {
-//     state = {
-//         email: "",
-//         password: "",
-//         message: "Search For A Book!"
-//     };
-//     handleInputChange = event => {
-//         const { name, value } = event.target;
-//         this.setState({
-//             [name]: value
-//         })
+class Login extends Component {
+    state = {
+        email: "",
+        password: "",
+        message: "Search For A Book!"
+    };
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
 
-//     };
-//     handleFormSubmit = event => {
-//         event.preventDefault();
-//         this.getBooks();
-//     };
+    };
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
+    };
 
-//     render() {
-//         return (
-//             <div className="App">
-//                 <LoginForm
-//                     handleInputChange={this.handleInputChange}
-//                     handleFormSubmit={this.handleFormSubmit}
-//                     q={this.state.q}
-//                 />
-//             </div>
-//         );
-//     }
-// }
-
-// export default Login;
+    render() {
+        return (
+            <div className="App">
+                <LoginForm
+                    handleInputChange={this.handleInputChange}
+                    handleFormSubmit={this.handleFormSubmit}
+                    q={this.state.q}
+                />
+            </div>
+        );
+    }
+}
+export default Login;
