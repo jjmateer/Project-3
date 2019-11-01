374f8b160faa156d099969a8ccd1e2b789862489
John Mateer <jj.mateer@live.com>
2019-10-31 22:41:16 -0700

    Carried over the jwt.sign from the register function in loginController and used it for the login function if the user enters the correct password.

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,16 +3,19 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 
 exports.register = function (req, res) {
+  //Check if there is a user in the database with same email.
   db.User.findOne({ email: req.body.email })
     .then(user => {
+      //If there isn't, create a new user with hashed password.
       if (!user) {
         let salt = bcrypt.genSaltSync(10);
         db.User.create({
           email: req.body.email,
           password: bcrypt.hashSync(req.body.password, salt),
         }).then(user => {
+          //Create JSON web token which is signed with the new user's info and expires in 1 hour.
           jwt.sign({ user }, 'secretkey', { expiresIn: 3600 }, (err, token) => {
-            if(err) throw err;
+            if (err) throw err;
             res.json({
               token,
               user
@@ -32,53 +35,25 @@ exports.register = function (req, res) {
 };
 
 exports.login = function (req, res) {
-  console.log(req.body)
-  const user = {
-    id: req.body._id,
-    username: req.body.username,
-    email: req.body.email
-  }
   // console.log(req.body)
-  jwt.sign({ user }, 'secretkey', { expiresIn: "1h" }, (err, token) => {
-    res.json({
-      token
+  db.User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        console.log("Login success.")
+        jwt.sign({ user }, 'secretkey', { expiresIn: 3600 }, (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+            user
+          });
+          console.log(`User info: ${user}`)
+          console.log(`User token: ${token}`)
+        })
+      } else {
+        console.log("Login failed.")
+      }
+    })
+    .catch(function (err) {
+      console.log(err)
     });
-  });
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // db.User.find({ email: req.body.email })
-  //   .then(function (user) {
-  //     console.log(user)
-  //     if (user && bcrypt.compareSync(req.body.password, user[0].password)) {
-  //       req.session.user = { username: user.email };
-  //       req.session.save(function () {
-  //         console.log("Login success.")
-  //         req.session.user = { id: user._id, username: user.email };
-  //         req.session.save(function() {
-  //           res.redirect("/");
-  //         });
-  //       });
-  //     } else {
-  //       console.log("Login failed.")
-  //       // console.log(req.body)
-  //     }
-  //   })
-  //   .catch(function (err) {
-  //     console.log(err)
-  //   });
