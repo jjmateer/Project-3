e86bd1f6d1913e5e0635919dbda48125e2778363
travis rodowick <travrodowick@yahoo.com>
2019-11-03 23:13:05 -0800

    changed the insert seeds function to reflect earlier changes

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -155,13 +155,13 @@ const itemSeeds = [
   }
 ];
 
-db.Book.remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+// db.Inventory.remove({})
+//   .then(() => db.Inventory.collection.insertMany(itemSeeds))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
