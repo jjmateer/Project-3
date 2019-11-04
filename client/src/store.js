2161eddaa2b8fd81dd7c1a237de32c3bd569c3ba
travis rodowick <travrodowick@yahoo.com>
2019-11-03 23:25:11 -0800

    tried setting up mongoDB bbut unable to get workign

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,6 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
-<<<<<<< HEAD
 const store = createStore(
   rootReducer,
   initialState,
@@ -13,10 +12,5 @@ const store = createStore(
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
-=======
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
->>>>>>> 2d2d5affa1f36ce3081e94abd94dbbdd2e007b86
 
 export default store;
