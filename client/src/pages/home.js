ff1e1b647700f7f4f1d93112e1f030e7a6d06685
John Mateer <jj.mateer@live.com>
2019-11-04 15:59:18 -0800

    Converted all page layout components to class based components, connected them to redux, and added static proptypes to allow access of the isAuthenticated prop for conditional rendering.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "../components/nav/navhome";
 import Homelayout from "../components/homelayout/homelayout"
+import { connect } from "react-redux";
+import { clearErrors } from "../actions/errorActions";
+import PropTypes from "prop-types";
 
 
-function Home() {
-    return (
+class Home extends Component {
+    state = {
+        email: "",
+        password: "",
+        message: null
+    };
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        error: PropTypes.object.isRequired,
+        clearErrors: PropTypes.func.isRequired
+    }
 
-        <div >
-            <Nav />
-            <h1 className="page-title">Home</h1>
-            < Homelayout />
-        </div>
-    );
+    render() {
+        return (
+
+            <div >
+                <Nav />
+                {this.props.isAuthenticated ? <h1>User logged in</h1> : <h1>User not logged in</h1>}
+                <h1 className="page-title">Home</h1>
+                < Homelayout />
+            </div>
+        );
+    }
 }
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
+})
 
-export default Home;
+export default connect(
+    mapStateToProps,
+    { clearErrors }
+)(Home);
