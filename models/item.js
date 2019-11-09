246e199327f293480b0eb72ed940d77924a213d1
travis rodowick <travrodowick@yahoo.com>
2019-11-09 11:09:48 -0800

    added categories to all the items and added items routing

--- models/item.js
+++ models/item.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-  product: { type: String, required: true },
+  item: { type: String, required: true },
   brand: { type: String, required: true },
   //need to change price data type into something that represents a dollar amount -- using string as temp
   price: { type: String, required: true },
+  category: { type: String, required: false },
   date: { type: Date, default: Date.now },
-  description: { type: String, required: true },
+  description: { type: String, required: false },
   image: { data: Buffer, contentType: String, required: false },
-  quantityInStock: { type: Number, required: true }
+  quantityInStock: { type: Number, required: false }
 });
 
 const Item = mongoose.model("Item", itemSchema);
--- scripts/seeds.js
+++ scripts/seeds.js
@@ -1,79 +1,84 @@
 const mongoose = require("mongoose");
 const Item = require("../models/item");
 
-mongoose.connect('mongodb://localhost/storefrontdb',
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect("mongodb://localhost/storefrontdb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback() {
   console.log("Connected to MongoDB.");
 });
 // This file empties the Item collection and inserts the items below
 
 const itemSeeds = [
   {
-    product: "Mac Book",
+    item: "Mac Book",
     brand: "Apple",
     price: "1099",
+    category: "laptop",
     description: "Pretty cool, if you like Apple...",
     image: "https://i.imgur.com/qIVwgvQ.jpg",
     quantityInStock: 100,
     date: new Date(Date.now())
   },
   {
-    product: "ThinkPad",
+    item: "ThinkPad",
     brand: "Lenovo",
     price: "999",
+    category: "laptop",
     description: "the ultimate computing machine..",
     image: "https://i.imgur.com/6mFoHuu.jpg",
     quantityInStock: 100,
     date: new Date(Date.now())
   },
   {
-    product: "iPad",
+    item: "iPad",
     brand: "Apple",
     price: "499",
+    category: "tablet",
     description: "Pretty cool, if you like Apple...",
     image: "https://i.imgur.com/1v35cu1.jpg",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Surface Pro",
+    item: "Surface Pro",
     brand: "Microsoft",
     price: "1499",
+    category: "tablet",
     description: "like an iPad, but costs more",
     image: "https://i.imgur.com/YKAe4WZ.jpg",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Pixel 4",
+    item: "Pixel 4",
     brand: "Google",
     price: "899",
+    category: "phone",
     description: "like an iPhone but costs less",
     image: "https://i.imgur.com/wpnC1fK.jpg",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "iPhone 11",
+    item: "iPhone 11",
     brand: "Apple",
     price: "1399",
+    category: "phone",
     description: "the gold standard in smart phones...",
     image: "https://i.imgur.com/FXm7cdK.jpg",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Code: The Hidden Language of Computer Hardware and Software",
+    item: "Code: The Hidden Language of Computer Hardware and Software",
     brand: "Charles Petzold",
     price: "23",
+    category: "book",
     description:
       "Using everyday objects and familiar language systems such as Braille and Morse code, author Charles Petzold weaves an illuminating narrative for anyone who’s ever wondered about the secret inner life of computers and other smart machines.",
     image: "https://i.imgur.com/FXm7cdK.jpg",
@@ -81,9 +86,10 @@ const itemSeeds = [
     date: new Date(Date.now())
   },
   {
-    product: "The Everything Store: Jeff Bezos and the Age of Amazon",
+    item: "The Everything Store: Jeff Bezos and the Age of Amazon",
     brand: "Brad Stone",
     price: "7",
+    category: "book",
     description:
       "thrilling fantasy novel of a man trying to take over the world",
     image: "",
@@ -91,46 +97,51 @@ const itemSeeds = [
     date: new Date(Date.now())
   },
   {
-    product: "Kindle",
+    item: "Kindle",
     brand: "Aamazon",
     price: "150",
+    category: "tablet",
     description: "it's like a book but better",
     image: "",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Apple Tv",
+    item: "Apple Tv",
     brand: "Apple",
     price: "150",
+    category: "accessories",
     description: "its like cable but better!",
     image: "",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Steve Jobs",
+    item: "Steve Jobs",
     brand: "Walter Isaacson",
     price: "16",
+    category: "book",
     description: "by the time this book is published it will be obsolete...",
     image: "",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "27 Full HD 1ms Monitor",
+    item: "27 Full HD 1ms Monitor",
     brand: "Phillips",
     price: "120",
+    category: "accessories",
     description:
       "Philips 27 inch Full HD 1ms LED Monitor, 1920 x 1080 resolution, 10M:1 contrast, 1ms response, VGA/HDMI, Tilt Adjustment",
-    image: "",
+    image: "accessories",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "Magsafe 2 Laptop Charger",
+    item: "Magsafe 2 Laptop Charger",
     brand: "Level Black",
     price: "17",
+    category: "accessories",
     description:
       "Input: 100-240V~1.5A @ 50Hz-60Hz & Output: 20V 4.25A max --Upgraded design with 2 additional USB ports --LED indicator light changes from amber to green when fully charged --Powers Li-Ion & Li-Poly batteries while off on or in sleep mode --Powers system without operating battery",
     image: "",
@@ -138,18 +149,20 @@ const itemSeeds = [
     date: new Date(Date.now())
   },
   {
-    product: "Nighthawk AC1900 Dual Band WiFi Gigabit Router (R7000)",
+    item: "Nighthawk AC1900 Dual Band WiFi Gigabit Router (R7000)",
     brand: "NETGEAR",
     price: "110",
+    category: "accessories",
     description: "802.11ac Dual Band Gigabit Router",
     image: "",
     quantityInStock: 200,
     date: new Date(Date.now())
   },
   {
-    product: "EX7300-100NAR Nighthawk AC2200 Plug-In WiFi Range Extender",
+    item: "EX7300-100NAR Nighthawk AC2200 Plug-In WiFi Range Extender",
     brand: "NETGEAR",
     price: "65",
+    category: "accessories",
     description:
       "The Nighthawk X4 Wi-Fi range extender boosts your existing Wi-Fi range with speeds up to 2200 Mbps",
     image: "",
@@ -157,9 +170,10 @@ const itemSeeds = [
     date: new Date(Date.now())
   },
   {
-    product: "D850",
+    item: "D850",
     brand: "Nikon",
     price: "2800",
+    category: "camera",
     description: "most badass camera ever... ",
     image: "",
     quantityInStock: 200,
@@ -167,8 +181,7 @@ const itemSeeds = [
   }
 ];
 
-Item
-  .deleteMany({})
+Item.deleteMany({})
   .then(() => Item.insertMany(itemSeeds))
   .then(data => {
     console.log(`${data.length} records inserted.`);
