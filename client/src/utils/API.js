3709fd498ad3d8944e855b7fff68ad4704e6d680
travis rodowick <travrodowick@yahoo.com>
2019-11-13 19:27:11 -0800

    changed api routes for search by category

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,59 +2,33 @@ import axios from "axios";
 
 export default {
   // Get all inventory
-<<<<<<< HEAD
   getInventory: function() {
-    return axios.get("http://localhost:3001/api/inventory");
-  },
-  // Get the item with the given id
-  getItem: function(id) {
-    return axios.get("http://localhost:3001/api/inventory/" + id);
-=======
-  getInventory: function () {
     return axios.get("/api/inventory");
   },
   // Get the item with the given id
-  getItem: function (id) {
+  getItem: function(id) {
     return axios.get("/api/inventory/" + id);
->>>>>>> 7275ea37e0f4858bebf7f6fde82e4f2e2eb20762
   },
 
   //get items by category
-  getItemCategory: function(category) {},
+  getItemCategory: function(category) {
+    return axios.post("/api/inventory", category);
+  },
   // Deletes the item with the given id
-<<<<<<< HEAD
   deleteItem: function(id) {
-    return axios.delete("http://localhost:3001/api/inventory/" + id);
+    return axios.delete("/api/inventory/" + id);
   },
   // Saves a item to the database
   saveItem: function(itemData) {
-    return axios.post("http://localhost:3001/api/inventory", itemData);
-  },
-  register: function(newUser) {
-    return axios.post("http://localhost:3001/api/login/r", newUser);
-  },
-  login: function(userInfo) {
-    return axios.post("http://localhost:3001/api/login/l", userInfo);
-  },
-  user: function(userInfo) {
-    return axios.get("http://localhost:3001/api/user", userInfo);
-=======
-  deleteItem: function (id) {
-    return axios.delete("/api/inventory/" + id); 
-  },
-  // Saves a item to the database
-  saveItem: function (itemData) {
     return axios.post("/api/inventory", itemData);
   },
-  register: function (newUser) {
+  register: function(newUser) {
     return axios.post("/api/login/r", newUser);
-
   },
-  login: function (userInfo) {
-    return axios.post("/api/login/l", userInfo)
+  login: function(userInfo) {
+    return axios.post("/api/login/l", userInfo);
   },
-  user: function (userInfo) {
-    return axios.get("/api/user", userInfo)
->>>>>>> 7275ea37e0f4858bebf7f6fde82e4f2e2eb20762
+  user: function(userInfo) {
+    return axios.get("/api/user", userInfo);
   }
 };
