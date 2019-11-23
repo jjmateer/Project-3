9aca2c8b5d881b3b66fc7248f053afab88b1159a
John Mateer <jj.mateer@live.com>
2019-11-23 00:00:25 -0800

    Reverting. Didn't work.

--- client/src/components/loginform/login-form.js
+++ client/src/components/loginform/login-form.js
@@ -3,13 +3,15 @@ import "./login-form.css";
 
 function LoginForm({ handleEmailChange, handlePasswordChange, handleFormSubmit }) {
     return (
-        <form className="auth-form">
-            <h3>Email :</h3>
-            <input className="form-control form-input-style" autoComplete="on" placeholder="  Email" onChange={handleEmailChange} required />
-            <h3>Password :</h3>
-            <input className="form-control form-input-style" autoComplete="on" type="password" placeholder="  Password" onChange={handlePasswordChange} required />
-            <input className="sub" type="submit" value="Submit" onClick={handleFormSubmit} />
-        </form>
+        <div>
+            <form className="login-form">
+                <h3>Email :</h3>
+                <input className="form-control form-input-style" autoComplete="on" placeholder="  Email" onChange={handleEmailChange} required />
+                <h3>Password :</h3>
+                <input className="form-control form-input-style" autoComplete="on" type="password" placeholder="  Password" onChange={handlePasswordChange} required />
+                <input className="sub" type="submit" value="Submit" onClick={handleFormSubmit} />
+            </form>
+        </div>
     )
 }
 
