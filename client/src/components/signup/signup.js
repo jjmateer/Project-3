185a089b89d1db9e68dde029d5c0d1c47edb47c0
John Mateer <jj.mateer@live.com>
2019-11-22 23:53:10 -0800

    Removing divs encasing the auth forms and names the forms themselves auth-form.

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -8,23 +8,22 @@ function SignupForm({ handleNameChange, handleEmailChange, handlePasswordChange,
 
             <h1 className="page-title">Sign Up</h1>
 
-            <div className="auth-form">
 
-                <form className="form-group">
-                    <h3>Name :</h3>
 
-                    <input className="form-control form-input-style" autoComplete="on" type="text" placeholder="  Name" onChange={handleNameChange} required />
+            <form className="auth-form">
+                <h3>Name :</h3>
 
-                    <h3>Email :</h3>
+                <input className="form-control form-input-style" autoComplete="on" type="text" placeholder="  Name" onChange={handleNameChange} required />
 
-                    <input className="form-control form-input-style" autoComplete="on" type="text" placeholder="  Email" onChange={handleEmailChange} required />
-                    <h3>Password :</h3>
+                <h3>Email :</h3>
 
-                    <input className="form-control" autoComplete="on" type="password" placeholder="  Password(6 characters minimum)" onChange={handlePasswordChange} required />
-                    <input className="sub" type="submit" value="Submit" onClick={handleFormSubmit} />
-                </form>
+                <input className="form-control form-input-style" autoComplete="on" type="text" placeholder="  Email" onChange={handleEmailChange} required />
+                <h3>Password :</h3>
 
-            </div>
+                <input className="form-control" autoComplete="on" type="password" placeholder="  Password(6 characters minimum)" onChange={handlePasswordChange} required />
+                <input className="sub" type="submit" value="Submit" onClick={handleFormSubmit} />
+            </form>
+
         </div>
     )
 }
