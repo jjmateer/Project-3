5749a76cafc92339b58811e1694f7023a25481e6
Melanie <melaniemok530@gmail.com>
2019-11-23 21:28:46 -0800

    adding items to seeds.js. Should have enough merchandise for all categories now.

--- client/src/components/loginform/login-form.js
+++ client/src/components/loginform/login-form.js
@@ -3,12 +3,12 @@ import "./login-form.css";
 
 function LoginForm({ handleEmailChange, handlePasswordChange, handleFormSubmit }) {
     return (
-        <div>
+        <div className="login-div">
             <form className="login-form">
                 <h3>Email :</h3>
                 <input className="form-control form-input-style" autoComplete="on" placeholder="  Email" onChange={handleEmailChange} required />
                 <h3>Password :</h3>
-                <input className="form-control form-input-style" autoComplete="on" type="password" placeholder="  Password" onChange={handlePasswordChange} required />
+                <input className="form-control form-input-style" autoComplete="on" type="password" placeholder="  Password " onChange={handlePasswordChange} required />
                 <input className="sub" type="submit" value="Submit" onClick={handleFormSubmit} />
             </form>
         </div>
