6158331680e3f059efbb47d10bdec96b167d2705
John Mateer <jj.mateer@live.com>
2019-12-03 01:23:07 -0800

    In process of adding mobile menu, checkpoint commit.

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -5,12 +5,21 @@ import { clearErrors } from "../../actions/errorActions";
 import PropTypes from "prop-types";
 import Search from "./search";
 import { logout } from "../../actions/authActions";
+import { slide as Menu } from 'react-burger-menu'
 import "./nav.css";
 
 class Nav extends Component {
-    state = {
-        message: null
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: null,
+            menuOpen: false,
+            text: ''
+        }
+    }
+    handleStateChange(state) {
+        this.setState({ menuOpen: state.isOpen })
+    }
     static propTypes = {
         isAuthenticated: PropTypes.bool,
         error: PropTypes.object.isRequired,
@@ -21,7 +30,15 @@ class Nav extends Component {
         return (
             <div className="global-header" >
                 <div className="global-header-left">
-                    <Link className="logo" to="/">RealTech</Link>
+                    <Link className="logo" to="/">RealTech
+                            <Menu noOverlay isOpen={this.state.menuOpen}
+                            onStateChange={(state) => this.handleStateChange(state)} id="hamburger">
+                            {this.props.isAuthenticated ? null : <Link to="/login">Log in</Link>}
+                            {this.props.isAuthenticated ? null : <Link to="/signup">Sign up</Link>}
+                            {this.props.isAuthenticated ? <Link to="/cart">Cart</Link> : null}
+                            {this.props.isAuthenticated ? <Link to="/" onClick={this.props.logout}>Logout</Link> : null}
+                        </Menu>
+                    </Link>
                 </div>
                 < Search />
                 <div className="global-header-right">
